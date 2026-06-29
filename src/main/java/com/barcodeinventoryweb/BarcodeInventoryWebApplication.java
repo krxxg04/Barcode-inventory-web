8ff14cc41fdb94288d10652a1a4c7668c77672e3
krxxg04 <u202312629@upc.edu.pe>
2026-06-29 17:52:18 -0500

    Load initial Spring Boot barcode inventory project

---
+++ src/main/java/com/barcodeinventoryweb/BarcodeInventoryWebApplication.java
@@ -0,0 +1,13 @@
+package com.barcodeinventoryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcodeInventoryWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarcodeInventoryWebApplication.class, args);
+    }
+
+}
